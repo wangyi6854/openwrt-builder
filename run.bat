46cf986f802563b83a2be4364be6f3f55bb840b4
Yi <wangyi6854@users.noreply.github.com>
2024-04-12 15:59:31 +0800

    Use docker volume to store source code.

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 
 IF [%1]==[] (
-    set map_openwrt_location=
+    set map_openwrt_location="-v openwrt_builder:/home/user/openwrt"
 ) ELSE (
     set map_openwrt_location="-v %1:/home/user/openwrt"
 )
